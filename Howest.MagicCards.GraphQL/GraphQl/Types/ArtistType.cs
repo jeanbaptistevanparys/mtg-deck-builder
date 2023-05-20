81f1ba95337a3f29dd8b4af49e4a10c964f60e8a
Jean-Baptiste Van Parys <jean-baptiste.van.parys@student.howest.be>
2023-05-20 14:49:28 +0200

    types

---
+++ Howest.MagicCards.GraphQL/GraphQl/Types/ArtistType.cs
@@ -0,0 +1,16 @@
+using GraphQL.Types;
+using Howest.MagicCards.DAL.Models.sql;
+
+namespace Howest.MagicCards.GraphQL.Types;
+
+public class ArtistType : ObjectGraphType<artist>
+{
+    public ArtistType()
+    {
+        Field(a => a.id, type: typeof(IdGraphType)).Description("id of the artist").Name("id");
+
+        Field(a => a.full_name, type: typeof(StringGraphType)).Description("name of the artist").Name("full_name");
+        Field(a => a.cards, type: typeof(ListGraphType<CardType>));
+    }
+    
+}
---
+++ Howest.MagicCards.GraphQL/GraphQl/Types/CardType.cs
@@ -0,0 +1,29 @@
+using GraphQL.Types;
+using Howest.MagicCards.DAL.Models.sql;
+using Howest.MagicCards.DAL.Repositories;
+
+namespace Howest.MagicCards.GraphQL.Types;
+
+public class CardType : ObjectGraphType<card>
+{
+    public CardType()
+    {
+        Field(c => c.id, type: typeof(IdGraphType)).Description("id of the card").Name("id");
+
+        Field(c => c.name, type: typeof(StringGraphType)).Description("name of the card").Name("name");
+        Field(c => c.mana_cost, type: typeof(StringGraphType)).Description("mana cost of the card").Name("mana_cost");
+        Field(c => c.converted_mana_cost, type: typeof(StringGraphType)).Description("converted mana cost of the card")
+            .Name("converted_mana_cost");
+        Field(c => c.power, type: typeof(StringGraphType)).Description("power of the card").Name("power");
+        Field(c => c.toughness, type: typeof(StringGraphType)).Description("toughness of the card").Name("toughness");
+        Field(c => c.card_colors, type: typeof(StringGraphType)).Description("card colors of the card")
+            .Name("card_colors");
+        Field(c => c.set_code, type: typeof(StringGraphType)).Description("set code of the card").Name("set_code");
+        Field(c => c.rarity_code, type: typeof(StringGraphType)).Description("rarity code of the card")
+            .Name("rarity_code");
+        Field<ArtistType>("artist");
+        Field(c => c.image, type: typeof(StringGraphType)).Description("image of the card").Name("image");
+        Field(c => c.original_image_url, type: typeof(StringGraphType)).Description("original image url of the card")
+            .Name("original_image_url");
+    }
+}
